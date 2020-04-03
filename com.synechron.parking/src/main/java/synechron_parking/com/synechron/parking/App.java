2b5d9dfb72ccc28a620effefc55abbfc0ad654a9
Gourav <gkgourav409@gmail.com>
2020-04-03 20:58:18 +0530

    first commit

---
+++ com.synechron.parking/src/main/java/synechron_parking/com/synechron/parking/App.java
@@ -0,0 +1,13 @@
+package synechron_parking.com.synechron.parking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ com.synechron.parking/src/main/java/synechron_parking/com/synechron/parking/model/Vehicle.java
@@ -0,0 +1,24 @@
+package synechron_parking.com.synechron.parking.model;
+
+public class Vehicle {
+	
+	private String registrationNumber;
+	private String color;
+	public Vehicle(String registrationNumber, String color) {
+		super();
+		this.registrationNumber = registrationNumber;
+		this.color = color;
+	}
+	public String getRegistrationNumber() {
+		return registrationNumber;
+	}
+	public void setRegistrationNumber(String registrationNumber) {
+		this.registrationNumber = registrationNumber;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+}
